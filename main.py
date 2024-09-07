c1d33610a1e7efacb096204cc17b1300ebeb064d
naensamble <naensamble@gmail.com>
2024-09-07 17:31:34 +0300

    fixed fight scenarios, minor changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncpg
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import BotCommand, FSInputFile, InputMediaAnimation
 from aiogram.filters import Command
+from aiogram.enums import ParseMode
 from datetime import datetime, timedelta
 import random
 import asyncio
@@ -15,7 +16,7 @@ load_dotenv()
 API_TOKEN = os.getenv('TOKEN')
 
 # настройки подключения к базе данных
-DB_HOST = os.getenv('POSTGRES_HOST')
+DB_HOST = os.getenv('POSTGRES_DB_HOST')
 DB_USER = os.getenv('POSTGRES_USER')
 DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 DB_NAME = os.getenv('POSTGRES_DB')
@@ -48,13 +49,13 @@ async def register_user(message: types.Message):
     async with pool.acquire() as connection:
         existing_user = await connection.fetchval('SELECT id FROM users WHERE telegram_id = $1', user_id)
         if existing_user:
-            await message.reply('Вы уже зарегистрированы.')
+            await message.reply('Ты уже зарегистрирован.')
         else:
             new_user_id = await connection.fetchval(
                 'INSERT INTO users (telegram_id, username) VALUES ($1, $2) RETURNING id', user_id, username
             )
             await connection.execute('INSERT INTO user_balance (user_id, points) VALUES ($1, 500)', new_user_id)
-            await message.reply('Вы успешно зарегистрированы!')
+            await message.reply('Ты успешно зарегистрирован!')
 
 # выбор пидора дня
 async def choose_pidor_of_the_day(message: types.Message):
@@ -67,7 +68,7 @@ async def choose_pidor_of_the_day(message: types.Message):
 
         if fighter_today:
             user = await connection.fetchrow('SELECT username FROM users WHERE id = $1', fighter_today['user_id'])
-            await message.reply(f'Согласно моей информации, по результатам сегодняшнего розыгрыша пидор дня: @{user["username"]}')
+            await message.reply(f'Согласно моей информации, по результатам сегодняшнего розыгрыша *пидор* дня: @{user["username"]}', parse_mode=ParseMode.MARKDOWN_V2)
         else:
             users = await connection.fetch('SELECT id, username FROM users')
             if not users:
@@ -88,7 +89,7 @@ async def choose_pidor_of_the_day(message: types.Message):
                 'SELECT message_text FROM messages WHERE message_type = $1 ORDER BY random() LIMIT 1', 'RESULT'
             )
             result_message = result_message_template.replace('{username}', f'@{chosen_user["username"]}')
-            await message.reply(result_message)
+            await message.reply(result_message, parse_mode=ParseMode.MARKDOWN_V2)
 
 # функция отвечающая за дуэли
 async def duel_command(message: types.Message):
@@ -100,14 +101,36 @@ async def duel_command(message: types.Message):
             logging.info(f'Challenger ID: {challenger_id}')
             
             if not challenger_id:
-                await message.reply('Вы не зарегистрированы. Пожалуйста, используйте команду /register, чтобы зарегистрироваться.')
+                await message.reply('Ты не зарегистрирован. Используй команду /register, чтобы зарегистрироваться.')
                 return
 
             challenged_id = None
             mentioned_username = None
-            
-            # проверка, если команда содержит упоминание другого пользователя
-            if len(message.text.split()) > 1:
+
+            # сценарий 1: ответ на сообщение
+            if message.reply_to_message:
+                logging.info('Reply to message found.')
+                challenged_id = await connection.fetchval(
+                    'SELECT id FROM users WHERE telegram_id = $1', message.reply_to_message.from_user.id)
+                logging.info(f'Challenged ID from reply: {challenged_id}')
+
+                if not challenged_id:
+                    await message.reply('Пользователь, которому ты бросил вызов, не зарегистрирован в игре.')
+                    logging.info('Challenged user is not registered.')
+                    return
+
+                # проверка, чтобы пользователь не мог вызвать сам себя на дуэль
+                if challenger_id == challenged_id:
+                    await message.reply('Ты не можешь вызвать на бой самого себя.')
+                    return
+                
+                await message.reply(
+                    f"@{message.reply_to_message.from_user.username}, *тебе бросили вызов*\! Поборешься с этим ♂jabroni♂\? \(/accept\)",
+                    parse_mode=ParseMode.MARKDOWN_V2
+                    )
+
+            # сценарий 2: упоминание другого пользователя
+            elif len(message.text.split()) > 1:
                 mentioned_username = message.text.split()[1].strip('@')
                 challenged_id = await connection.fetchval(
                     'SELECT id FROM users WHERE username = $1', mentioned_username)
@@ -117,54 +140,37 @@ async def duel_command(message: types.Message):
                     await message.reply(f'Пользователь @{mentioned_username} не зарегистрирован в игре.')
                     return
 
-            # проверка на наличие ответного сообщения
-            elif message.reply_to_message:
-                logging.info('Reply to message found.')
-                challenged_id = await connection.fetchval(
-                    'SELECT id FROM users WHERE telegram_id = $1', message.reply_to_message.from_user.id)
-                logging.info(f'Challenged ID from reply: {challenged_id}')
-
-                if not challenged_id:
-                    await message.reply('Пользователь, которому вы бросили вызов, не зарегистрирован в игре.')
-                    logging.info('Challenged user is not registered.')
+                # проверка, чтобы пользователь не мог вызвать сам себя на дуэль
+                if challenger_id == challenged_id:
+                    await message.reply('Ты не можешь вызвать на бой самого себя.')
                     return
             
+                await message.reply(f"@{mentioned_username}, *тебе бросили вызов*\! Поборешься с этим ♂jabroni♂\? \(/accept\)",
+                    parse_mode=ParseMode.MARKDOWN_V2
+                    )
+            
+            # сценарий 3: открытая дуэль
             else:
-                # создание открытой дуэли
-                await message.reply('Вы создали открытую дуэль! Любой зарегистрированный пользователь может принять вызов командой /accept.')
+                logging.info('Open duel created.')
+                await message.reply('Я новый *♂dungeon master♂*\! Кто не согласен, отзовись или молчи вечно\! /accept\.', parse_mode=ParseMode.MARKDOWN_V2)
                 result = await connection.execute(
-                    'INSERT INTO duel_state (challenger_id, challenged_id) VALUES ($1, NULL)', 
-                    challenger_id
+                    'INSERT INTO duel_state (challenger_id, challenged_id, duel_type) VALUES ($1, $2, $3)', 
+                    challenger_id, None, 'open'
                 )
-                
                 logging.info(f'Open Duel Insert Result: {result}')
                 return
-            
-            # проверка, чтобы пользователь не мог вызвать сам себя на дуэль
-            if challenger_id == challenged_id:
-                await message.reply('Вы не можете вызвать на бой самого себя.')
-                return
 
-            if mentioned_username:
-                await message.reply(f'@{mentioned_username}, вас вызвали побороться! Примете вызов? (/accept)')
-            else:
-                await message.reply(f'@{message.reply_to_message.from_user.username}, вас вызвали на бой! Примете вызов? (/accept)')
-
-            result = await connection.execute(
-                'INSERT INTO duel_state (challenger_id, challenged_id) VALUES ($1, $2)', 
-                challenger_id, challenged_id
-            )
-            
-            logging.info(f'Duel Insert Result: {result}')
-            if result == 'INSERT 0 1':
-                logging.info(f'Duel created: Challenger {challenger_id}, Challenged {challenged_id}')
-            else:
-                logging.error(f'Failed to insert duel: Challenger {challenger_id}, Challenged {challenged_id}, Result: {result}')
+            # добавление дуэли в базу (кроме открытой дуэли)
+            if challenged_id is not None:
+                result = await connection.execute(
+                    'INSERT INTO duel_state (challenger_id, challenged_id, duel_type) VALUES ($1, $2, $3)', 
+                    challenger_id, challenged_id, 'specific'
+                )
+                logging.info(f'Duel Insert Result: {result}')
 
         except Exception as e:
             logging.error(f'Error in duel_command: {e}')
-            await message.reply('Произошла ошибка при создании дуэли. Попробуйте еще раз.')
-
+            await message.reply('Произошла ошибка при создании дуэли. Попробуй еще раз.')
 
 async def accept_duel_command(message: types.Message):
     await create_db_pool()
@@ -176,30 +182,55 @@ async def accept_duel_command(message: types.Message):
             )
 
             if not user_id:
-                await message.reply('Вы не зарегистрированы в игре. Пожалуйста, используйте команду /register, чтобы зарегистрироваться.')
+                await message.reply('Ты не зарегистрирован в игре. Используй команду /register, чтобы зарегистрироваться.')
                 return
             
             current_time = datetime.utcnow()
 
-            # получение информации об открытой дуэли
+            # СЦЕНАРИЙ 1: принятие вызова на конкретную дуэль (когда пользователь был вызван другим пользователем)
+            logging.info('Searching for specific duel where user was challenged.')
             duel_info = await connection.fetchrow(
-                'SELECT * FROM duel_state WHERE challenged_id IS NULL AND created_at > $1',
-                current_time - timedelta(minutes=5)
+                'SELECT * FROM duel_state WHERE challenged_id = $1 AND duel_type = $2 AND created_at > $3', 
+                user_id, 'specific', current_time - timedelta(minutes=1)
             )
 
-            if not duel_info:
-                await message.reply('Нет доступных открытых дуэлей для принятия.')
-                return
+            # если пользователь вызван на конкретную дуэль
+            if duel_info:
+                logging.info('Specific duel found, accepting...')
 
-            # обновление записи, добавление challenged_id
-            await connection.execute(
-                'UPDATE duel_state SET challenged_id = $1 WHERE id = $2',
-                user_id, duel_info['id']
-            )
+                # проверка: нельзя принять дуэль, созданную самим собой
+                if duel_info['challenger_id'] == user_id:
+                    await message.reply('Ты не можешь принять бой с самим собой.')
+                    return
+
+                # здесь мы не обновляем challenged_id, так как оно уже установлено в вызове дуэли
+        
+            # СЦЕНАРИЙ 2: принятие открытой дуэли
+            else:
+                logging.info('No specific duel found, searching for open duel.')
+                duel_info = await connection.fetchrow(
+                    'SELECT * FROM duel_state WHERE challenged_id IS NULL AND created_at > $1 AND duel_type = $2',
+                    current_time - timedelta(minutes=1), 'open'
+                )
+
+                if not duel_info:
+                    await message.reply('Нет доступных дуэлей для принятия.')
+                    return
+                
+                # проверка: нельзя принять открытую дуэль от самого себя
+                if duel_info['challenger_id'] == user_id:
+                    await message.reply('Ты не можешь принять бой с самим собой.')
+                    return
+
+                # обновляем запись, добавляем challenged_id
+                await connection.execute(
+                    'UPDATE duel_state SET challenged_id = $1 WHERE id = $2',
+                    user_id, duel_info['id']
+                )
 
             await message.reply('Борьба началась!')
-            
-            # отправка первой GIF-изображения
+
+            # отправка GIF-изображений
             gif_folder_path = './gifs'
             all_gifs = [os.path.join(gif_folder_path, file) for file in os.listdir(gif_folder_path) if file.endswith('.gif')]
             gif_files = random.sample(all_gifs, 3)
@@ -256,8 +287,9 @@ async def rating(message: types.Message):
         if not stats:
             await message.reply('Статистика пока пуста.')
         else:
-            stats_message = f"Рейтинг пидоров (данные актуальны на {current_year} год):\n" + \
-                            "\n".join([f"@{stat['username']}: {stat['chosen_count']} раз" for stat in stats])
+            stats_message = f"Рейтинг пидоров (данные актуальны на {current_year} год):\n"
+            for idx, stat in enumerate(stats, start=1):
+                stats_message += f"{idx}. {stat['username']}: {stat['chosen_count']} раз\n"
             await message.reply(stats_message)
 
 async def show_fight_stats(message: types.Message):
@@ -283,13 +315,13 @@ async def show_fight_stats(message: types.Message):
         )
 
         if not stats:
-            await message.reply('Статистика боев пока пуста.')
+            await message.reply('Статистика поединков пока пуста.')
         else:
             stats_message = "Статистика по боям:\n"
-            for stat in stats:
-                stats_message += (f"@{stat['username']}: Победы: {stat['wins']}, "
+            for idx, stat in enumerate(stats, start=1):
+                stats_message += (f"{idx}. {stat['username']}: Победы: {stat['wins']}, "
                                   f"Поражения: {stat['losses']}, "
-                                  f"Количество ♂️semen: {stat['current_balance']}\n")
+                                  f"Количество ♂️semen♂️: {stat['current_balance']}\n\n")
             await message.reply(stats_message)
 
 async def main():
